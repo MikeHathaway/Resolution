82c7635bd174f49a039ed054b305c247b508b0f3
Mike Hathaway <mahathaway93@gmail.com>
2019-08-18 16:30:35 -0700

    add contract events for logging

--- State/contracts/Jhum.sol
+++ State/contracts/Jhum.sol
@@ -9,12 +9,15 @@ import "../node_modules/openzeppelin-solidity/contracts/payment/escrow/Condition
 contract Jhum is ConditionalEscrow {
 
 	uint256 goal;
+	uint256 wageredAmount;
 	bytes32 duration;
 	address validator;
-	address payable private donationTarget;
+	address payable donationTarget;
 
     mapping(uint256 => address) private _participants;
-    
+
+	event Burn(uint256 _goal, address indexed payable donationTarget);
+
 	constructor(uint256 _goal, address _validator, address payable _donationTarget) public {
 		goal = _goal;
 		validator = _validator;
@@ -34,14 +37,24 @@ contract Jhum is ConditionalEscrow {
 		}	
 	}
 
-	// if validator sends signed message that terms have are being met, lengthen period to escrow burn 
+	// if validator sends signed message that terms are being met, lengthen period to escrow burn 
 	function restartTimer() private view {
 		require(msg.data.length != 0);
+		
+		// TODO: use block.number to identify time
+	}
 
+	function shouldBurn() private returns (bool) {
+	
 	}
 
 	// if validator does not sign message verifying conditions are met, send contract value to specified contract
-	function burnValue() public {
-		// send to donation target	
+	function burnValue(address payable _donationTarget) public returns (bool) {
+		require(shouldBurn());		
+		// log burn to blockchain for display to clients (TODO: Parameterize event fields)
+		emit Burn(123, 0x0fdaf8757F74e5CAE7DcAd5c0A4A6c27f13eC7FF);	
+	
+		// send to donation target
+		return donationTarget.send(wageredAmount);
 	}
 }
