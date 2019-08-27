b2757511afee3c4493a24cc263ecfcb6e52095d6
Mike Hathaway <mahathaway93@gmail.com>
2019-08-26 23:44:32 -0700

    cleanup call; add param for sending msg.value

--- State/callContract.js
+++ State/callContract.js
@@ -3,18 +3,13 @@ const resolutionABI = require('./contractConfiguration/Resolution.json');
 const fs = require('fs');
 
 function connectToContract(){
-	// Connect to the network
 	const provider = new ethers.providers.JsonRpcProvider()
 
 	const contractAddress = "0xB869DcA31a2817fa814B78c9c5cE6A52f6311782"
 
-	// We connect to the Contract using a Provider, so we will only
-	// have read-only access to the Contract
 	const contract = new ethers.Contract(contractAddress, resolutionABI.abi, provider);
 
-	const mnemonic = fs.readFileSync('./.secret', 'utf8');
-
-	const privateKey = fs.readFileSync('./.key', 'utf8'); 
+	const privateKey = fs.readFileSync('./.key.txt', 'utf8').replace(/[\n\r]/g, ''); 
 	const wallet = new ethers.Wallet(privateKey, provider);
 	
 	const contractWithSigner = contract.connect(wallet);
@@ -30,32 +25,13 @@ function connectToContract(){
 				donationTarget: '0x0fdaf8757F74e5CAE7DcAd5c0A4A6c27f13eC7FF'
 			};
 
-			const methodParams = [
-				"Dank",
-				"420",
-				'0x0fdaf8757F74e5CAE7DcAd5c0A4A6c27f13eC7FF'
-			];
-			const transactionParams = {
-				// Required unless deploying a contract (in which case omit)
-				to: '0xB869DcA31a2817fa814B78c9c5cE6A52f6311782',  // the target address or ENS name
-
-				// These are optional/meaningless for call and estimateGas
-				nonce: 0,           // the transaction nonce
-				gasLimit: 0,        // the maximum gas this transaction may spend
-				gasPrice: 0,        // the price (in wei) per unit of gas
-
-				// These are always optional (but for call, data is usually specified)
-				data: "0x",         // extra data for the transaction, or input for call
-				value: .5,           // the amount (in wei) this transaction is sending
-				chainId: 3          // the network ID; usually added by a signer	
-			};
-
 			const tx = await contractWithSigner.createResolution(
 				"Dank", 
 				Math.floor(Math.random()*1000).toString(), 
 				'0x0fdaf8757F74e5CAE7DcAd5c0A4A6c27f13eC7FF', 
 				'0x0fdaf8757F74e5CAE7DcAd5c0A4A6c27f13eC7FF', 
-				'0x0fdaf8757F74e5CAE7DcAd5c0A4A6c27f13eC7FF'
+				'0x0fdaf8757F74e5CAE7DcAd5c0A4A6c27f13eC7FF',
+				{value: 100000}
 			);
 
 			tx.wait().then(receipt => {
